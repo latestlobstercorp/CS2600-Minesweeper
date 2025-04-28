ee67d26660646e698a2663ea244c517933cbb99e
thy-hoang <thyh2319@gmail.com>
2025-04-28 15:28:49 -0700

    Created Board.c and GameBoard.c

---
+++ GameBoard.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+int main() {
+    int size = 5; // Size of the board
+    int numMines = 3; // Number of mines to place
+
+    // Create the board
+    struct Board *b = createBoard(size);
+
+    // // Initialize mines
+    // initializeMines(b, numMines);
+
+    // Print the board
+    printBoard(b);
+
+    // Free memory (not shown here for brevity)
+
+    return 0;
+}
