f95c6d2a78ad6278b9eac52a96e818d44a028d3f
walsh0605 <144873494+walsh0605@users.noreply.github.com>
2025-04-30 16:01:47 -0700

    Add files via upload

---
+++ boardKW.c
@@ -0,0 +1,74 @@
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+
+struct Board {
+    int **board;
+    int size;
+};
+
+struct Board *createBoard(int size) {
+    struct Board *b = malloc(sizeof(struct Board));
+    b->size = size;
+    b->board = malloc(size * sizeof(int *));
+    for (int i = 0; i < size; i++) {
+        b->board[i] = malloc(size * sizeof(int));
+        for (int j = 0; j < size; j++) {
+            b->board[i][j] = 0; // Initialize all cells to 0 (empty)
+        }
+    }
+    return b;
+}
+
+void printBoard(struct Board *b) {
+    for (int i = 0; i < b->size; i++) {
+        for (int j = 0; j < b->size; j++) {
+            if (b->board[i][j] == 0) { // 0 represents an empty cell
+                printf("[]");
+            } else if (b->board[i][j] == 1) { // 1 represents a revealed cell
+                printf("-");
+            } else if (b->board[i][j] == 2) { // 2 represents a mine
+                printf("X"); 
+            }
+        }
+        printf("\n");
+    }
+}
+
+void initializeMines(struct Board *b, int numMines) {
+    int placedMines = 0;
+    while (placedMines < numMines) {
+        int x = rand() % b->size;
+        int y = rand() % b->size;
+        if (b->board[x][y] == 0) { // If the cell is empty
+            b->board[x][y] = 2; // Place a mine (2)
+            placedMines++;
+        }
+    }
+}
+
+int main() {
+    int boardSize;
+    int numMines;
+
+    printf("%s", "Please Enter Width of Board, i.e. 5 :\n");
+    scanf("%d", &boardSize);
+    printf("\n");
+
+    struct Board *b = createBoard(boardSize);
+
+    numMines = boardSize;
+
+    initializeMines(b, numMines);
+    printBoard(b);
+
+    // Free allocated memory 
+    for (int i = 0; i < boardSize; i++) {
+        free(b->board[i]);
+    }
+    free(b->board);
+    free(b);
+
+    return 0;
+}
